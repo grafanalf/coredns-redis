ff1241d7d9a5f364afe221b4e4376c400db592f3
Felipe <89969424+grafanalf@users.noreply.github.com>
2023-02-14 02:07:43 +0100

    Consolidate record handling code

--- record/record.go
+++ record/record.go
@@ -1,5 +1,7 @@
 package record
 
+import "net"
+
 // Records holds the location records for a zone
 type Records struct {
 	Ttl int `json:"ttl",omitempty`
@@ -15,4 +17,68 @@ type Records struct {
 	SRV   []SRV   `json:"SRV,omitempty"`
 	PTR   []PTR   `json:"PTR,omitempty"`
 	CAA   []CAA   `json:"CAA,omitempty"`
+}
+
+// SOA RDATA (https://tools.ietf.org/html/rfc1035#section-3.3.13)
+type SOA struct {
+	Ttl     int    `json:"ttl"`
+	MName   string `json:"mname"`
+	RName   string `json:"rname"`
+	Serial  uint32 `json:"serial"`
+	Refresh uint32 `json:"refresh"`
+	Retry   uint32 `json:"retry"`
+	Expire  uint32 `json:"expire"`
+	MinTtl  uint32 `json:"min_ttl"`
+}
+
+type A struct {
+	Ttl int    `json:"ttl"`
+	Ip  net.IP `json:"ip"`
+}
+type AAAA struct {
+	Ttl int    `json:"ttl"`
+	Ip  net.IP `json:"ip"`
+}
+
+type TXT struct {
+	Ttl  int    `json:"ttl"`
+	Text string `json:"text"`
+}
+
+type CNAME struct {
+	Ttl  int    `json:"ttl"`
+	Host string `json:"host"`
+}
+
+// NS RDATA (https://tools.ietf.org/html/rfc1035#section-3.3.11)
+type NS struct {
+	Ttl  int    `json:"ttl"`
+	Host string `json:"host"`
+}
+
+// MX RDATA (https://tools.ietf.org/html/rfc1035#section-3.3.9)
+type MX struct {
+	Ttl        int    `json:"ttl"`
+	Host       string `json:"host"`
+	Preference uint16 `json:"preference"`
+}
+
+// SRV RDATA (https://tools.ietf.org/html/rfc2782)
+type SRV struct {
+	Ttl      int    `json:"ttl"`
+	Priority uint16 `json:"priority"`
+	Weight   uint16 `json:"weight"`
+	Port     uint16 `json:"port"`
+	Target   string `json:"target"`
+}
+type PTR struct {
+	Ttl  int    `json:"ttl"`
+	Name string `json:"name"`
+}
+
+type CAA struct {
+	Ttl   int    `json:"ttl"`
+	Flag  uint8  `json:"flag"`
+	Tag   string `json:"tag"`
+	Value string `json:"value"`
 }
